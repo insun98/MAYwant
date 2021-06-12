895a37e73dcdd3c80581b6b8c54fc08f93298342
Insun98 <ysehisforever@gmail.com>
2021-06-12 21:49:38 +0900

    Update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express')
 var app = express()
-app.use(express.static(__dirname + '/36-rabbit')); 
+ 
+app.use(express.static(__dirname + '/temp'));
 app.get('/', function(req, res) {
    res.send('<h1>Helloooo</h1>')
-})
- 
+}) 
 app.listen(8888,function(){
    console.log("server started at 8888")
 })
